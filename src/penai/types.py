86b07fc5d72ce2405add1c451cc7ae7cc75074a5
Michael Panchenko <m.panchenko@appliedai.de>
2024-05-27 13:45:13 +0200

    Added stupid workaround for docs to work

--- src/penai/types.py
+++ src/penai/types.py
@@ -1,4 +1,12 @@
 import os
 from pathlib import Path
 
+
+# TODO: sphinx is too dumb to render modules without at least one function
+#  and I couldn't figure out how to suppress the specific warning
+#  Remove once the module is less empty
+def dummy() -> None:
+    """Dummy function to make sphinx happy."""
+
+
 PathLike = str | Path | os.PathLike[str]
