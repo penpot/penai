025e932f0f2b2b311c96876ae545bd138cc9ed80
Kristian Klemon <k.klemon@appliedai.de>
2024-05-28 16:07:07 +0200

    Implement test for ResvgRenderer

--- test/penai/test_render.py
+++ test/penai/test_render.py
@@ -1,39 +1,69 @@
+import tempfile
 from pathlib import Path
 
 import numpy as np
 from PIL import Image
 
-from penai.render import BaseSVGRenderer, ChromeSVGRenderer
+from penai.render import BaseSVGRenderer, ChromeSVGRenderer, ResvgRenderer
 
 
-def _test_chrome_svg_renderer(
-    renderer: BaseSVGRenderer, example_svg_path: Path, example_png: Path,
+def _test_svg_renderer(
+    renderer: BaseSVGRenderer,
+    example_svg_path: Path,
+    example_png: Path,
 ) -> None:
     ref_png = Image.open(example_png)
     cmp_png = renderer.render_svg_file(example_svg_path)
 
     assert ref_png.size == cmp_png.size
 
-    ref_png = ref_png.convert("RGB")
+    if not renderer.SUPPORTS_ALPHA:
+        ref_png = ref_png.convert("RGB")
 
     ref_data = np.array(ref_png) / 255.0
     cmp_data = np.array(cmp_png) / 255.0
 
-    # We compare a properly _exported_ SVG against a screenshot
-    # The two versions are therefore not expected to match pixel-perfectly
-    assert ((ref_data - cmp_data) ** 2).mean() < 1e-3
+    if ((ref_data - cmp_data) ** 2).mean() > 5e-3:
+        with tempfile.TemporaryDirectory(delete=False) as tmp_dir:
+            tmp_dir_path = Path(tmp_dir)
+            ref_path = tmp_dir_path / "ref.png"
+            cmp_path = tmp_dir_path / "cmp.png"
+
+            ref_png.save(ref_path)
+            cmp_png.save(cmp_path)
+
+            raise AssertionError(
+                f"Images do not match. Saved to  and {cmp_path} respectively for visual inspection.",
+            )
 
 
-class TestRendering:
-    @staticmethod
-    def test_chrome_svg_renderer(example_svg_path: Path, example_png: Path) -> None:
+class TestChromeSVGRenderer:
+    def test_chrome_svg_renderer(
+        self,
+        example_svg_path: Path,
+        example_png: Path,
+    ) -> None:
         renderer = ChromeSVGRenderer()
 
-        _test_chrome_svg_renderer(renderer, example_svg_path, example_png)
+        _test_svg_renderer(renderer, example_svg_path, example_png)
 
         renderer.teardown()
 
-    @staticmethod
-    def test_chrome_svg_renderer_context_manager(example_svg_path: Path, example_png: Path) -> None:
+    def test_chrome_svg_renderer_context_manager(
+        self,
+        example_svg_path: Path,
+        example_png: Path,
+    ) -> None:
         with ChromeSVGRenderer.create_renderer() as renderer:
-            _test_chrome_svg_renderer(renderer, example_svg_path, example_png)
+            _test_svg_renderer(renderer, example_svg_path, example_png)
+
+
+class TestResvgRenderer:
+    def test_resvg_renderer(
+        self,
+        example_svg_path: Path,
+        example_png: Path,
+    ) -> None:
+        renderer = ResvgRenderer()
+
+        _test_svg_renderer(renderer, example_svg_path, example_png)
