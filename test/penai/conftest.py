f3b1ed5d138cd2835b80c9a4016c53422e926ee4
Kristian Klemon <k.klemon@appliedai.de>
2024-05-31 16:17:04 +0200

    Add test for bounding box derivation

--- test/penai/conftest.py
+++ test/penai/conftest.py
@@ -1,11 +1,15 @@
+from collections.abc import Generator
 from datetime import datetime
 from pathlib import Path
+from typing import Any
 
 import pytest
 from pytest import MonkeyPatch
+from selenium.webdriver.remote.webdriver import WebDriver
 
 from penai.config import top_level_directory
 from penai.types import PathLike
+from penai.utils.web_drivers import create_chrome_web_driver
 
 
 @pytest.fixture(autouse=True)
@@ -47,3 +51,9 @@ def log_dir() -> Path:
     session_log_dir = log_dir_root / datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     session_log_dir.mkdir(parents=True)
     return session_log_dir
+
+
+@pytest.fixture(scope="session")
+def chrom_web_driver() -> Generator[WebDriver, Any, Any]:
+    with create_chrome_web_driver() as driver:
+        yield driver
