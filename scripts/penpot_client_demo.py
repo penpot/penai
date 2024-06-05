416d34d0148fa973f0b78be403253b1eb7eee11a
Dominik Jain <d.jain@appliedai.de>
2024-06-05 12:54:10 +0200

    Determine UUIDs programmatically

--- scripts/penpot_client_demo.py
+++ scripts/penpot_client_demo.py
@@ -1,10 +1,18 @@
 from penai.client import PenpotClient
+from penai.registries.projects import SavedPenpotProject
 
 if __name__ == '__main__':
+    penpot_project = SavedPenpotProject.INTERACTIVE_MUSIC_APP.load(pull=True)
+    project_id = "15586d98-a20a-8145-8004-69dd979da070"
+
+    main_file = penpot_project.get_main_file()
+    page = main_file.get_page_by_name("Interactive music app")
+    shape = page.svg.get_shape_by_name("ic_equalizer_48px-1")
+
     client = PenpotClient.create_default()
-    shape = client.get_shape(
-        project_id="15586d98-a20a-8145-8004-69dd979da070",
-        file_id="cdedfc7e-d457-80ce-8004-6b19bad0cffe",
-        page_id="cdedfc7e-d457-80ce-8004-6b19bb6ff2c8",
-        shape_id="cdedfc7e-d457-80ce-8004-6b19c3a99be9"
+    result = client.get_shape(
+        project_id=project_id,
+        file_id=main_file.id,
+        page_id=page.id,
+        shape_id=shape.id
     )
