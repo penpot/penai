60901775f2d8f1e44cd2b0621244033d98030988
Kristian Klemon <k.klemon@appliedai.de>
2024-06-06 12:12:13 +0200

    Implement explicit size specification for svg renderer

--- test/penai/test_renderers.py
+++ test/penai/test_renderers.py
@@ -1,66 +1,86 @@
+from collections.abc import Iterable
 from pathlib import Path
 
 import numpy as np
+import pytest
 from PIL import Image
+from pytest import FixtureRequest
 
 from penai.render import BaseSVGRenderer, ResvgRenderer, WebDriverSVGRenderer
 
 
-def _test_svg_renderer(
-    renderer: BaseSVGRenderer,
-    example_svg_path: Path,
-    example_png_path: Path,
-    log_dir: Path,
-) -> None:
-    ref_png = Image.open(example_png_path)
-    cmp_png = renderer.render_svg_file(example_svg_path)
+@pytest.fixture()
+def chrome_web_driver() -> Iterable[BaseSVGRenderer]:
+    with WebDriverSVGRenderer.create_chrome_renderer() as renderer:
+        yield renderer
 
-    assert ref_png.size == cmp_png.size
 
-    if not renderer.SUPPORTS_ALPHA:
-        ref_png = ref_png.convert("RGB")
+@pytest.fixture()
+def resvg_renderer() -> Iterable[BaseSVGRenderer]:
+    renderer = ResvgRenderer()
+    yield renderer
+    renderer.teardown()
 
-    ref_data = np.array(ref_png) / 255.0
-    cmp_data = np.array(cmp_png) / 255.0
 
-    diff = ((ref_data - cmp_data) ** 2).mean()
-
-    # resvg uses a different fallback font so we need to have the tolerance
-    # slightly higher than for the Chrome renderer.
-    # For some cases, presumbly ones with lots of text, this might break entirely.
-    # In the long-term, however, the font issue will be fixed and the tolerance might
-    # be lowered again.
-    if diff > 5e-3:
-        ref_path = log_dir / "ref.png"
-        cmp_path = log_dir / "cmp.png"
-
-        ref_png.save(ref_path)
-        cmp_png.save(cmp_path)
-
-        raise AssertionError(
-            f"Images do not match. Saved to reference and generated image to {ref_path} and {cmp_path} for visual inspection.",
-        )
+@pytest.fixture(params=["chrome_web_driver", "resvg_renderer"])
+def renderer(request: FixtureRequest) -> BaseSVGRenderer:
+    return request.getfixturevalue(request.param)
 
 
 class TestSVGRenderers:
-    def test_resvg_renderer(
+    def test_rendering(
         self,
+        renderer: BaseSVGRenderer,
         example_svg_path: Path,
         example_png_path: Path,
         log_dir: Path,
     ) -> None:
+        ref_png = Image.open(example_png_path)
 
-        renderer = ResvgRenderer()
+        cmp_png = renderer.render_svg_file(example_svg_path)
 
-        _test_svg_renderer(renderer, example_svg_path, example_png_path, log_dir)
+        assert ref_png.size == cmp_png.size
 
-        renderer.teardown()
+        if not renderer.SUPPORTS_ALPHA:
+            ref_png = ref_png.convert("RGB")
 
-    def test_chrome_web_driver_svg_renderer(
-        self,
-        example_svg_path: Path,
-        example_png_path: Path,
-        log_dir: Path,
+        ref_data = np.array(ref_png) / 255.0
+        cmp_data = np.array(cmp_png) / 255.0
+
+        diff = ((ref_data - cmp_data) ** 2).mean()
+
+        # resvg uses a different fallback font so we need to have the tolerance
+        # slightly higher than for the Chrome renderer.
+        # For some cases, presumbly ones with lots of text, this might break entirely.
+        # In the long-term, however, the font issue will be fixed and the tolerance might
+        # be lowered again.
+        if diff > 5e-3:
+            ref_path = log_dir / "ref.png"
+            cmp_path = log_dir / "cmp.png"
+
+            ref_png.save(ref_path)
+            cmp_png.save(cmp_path)
+
+            raise AssertionError(
+                f"Images do not match. Saved to reference and generated image to {ref_path} and {cmp_path} for visual inspection.",
+            )
+
+    def test_explicit_size_specification(
+        self, renderer: BaseSVGRenderer, example_svg_path: Path,
     ) -> None:
-        with WebDriverSVGRenderer.create_chrome_renderer() as renderer:
-            _test_svg_renderer(renderer, example_svg_path, example_png_path, log_dir)
+        img = renderer.render_svg_file(example_svg_path)
+
+        orig_aspect_ratio = img.size[0] / img.size[1]
+
+        img = renderer.render_svg_file(example_svg_path, width=100, height=100)
+        assert img.size == (100, 100)
+
+        img = renderer.render_svg_file(example_svg_path, width=100)
+        assert img.size[0] == 100
+        assert img.size[0] >= img.size[1] if orig_aspect_ratio > 1 else img.size[0] <= img.size[1], \
+            f"Original aspect ratio: {orig_aspect_ratio}, new size: {img.size}"
+
+        img = renderer.render_svg_file(example_svg_path, height=100)
+        assert img.size[1] == 100
+        assert img.size[0] >= img.size[1] if orig_aspect_ratio > 1 else img.size[0] <= img.size[1], \
+            f"Original aspect ratio: {orig_aspect_ratio}, new size: {img.size}"
