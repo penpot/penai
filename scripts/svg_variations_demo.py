455136090605e2d308d07555581e2dbc45385976
Dominik Jain <d.jain@appliedai.de>
2024-06-06 15:29:04 +0200

    Add SVG post-processing to avoid overlapping identifiers, allowing revisions to be rendered within the same notebook

--- scripts/svg_variations_demo.py
+++ scripts/svg_variations_demo.py
@@ -3,7 +3,7 @@ from penai.registries.web_drivers import RegisteredWebDriver
 from penai.variations.svg_variations import SVGVariationsGenerator
 
 if __name__ == '__main__':
-    saved_penpot_project = SavedPenpotProject.INTERACTIVE_MUSIC_APP#
+    saved_penpot_project = SavedPenpotProject.INTERACTIVE_MUSIC_APP
     penpot_project = saved_penpot_project.load(pull=True)
     main_file = penpot_project.get_main_file()
     page = main_file.get_page_by_name("Interactive music app")
@@ -11,3 +11,6 @@ if __name__ == '__main__':
     shape = page.svg.get_shape_by_name("ic_equalizer_48px-1")
 
     var_gen = SVGVariationsGenerator(shape=shape, semantics="equalizer")
+    variations = var_gen.create_variations()
+    variations2 = variations.revise("Modify these variations such that they all consider shape changes.")
+
