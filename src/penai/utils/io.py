cf4237ce509b3e2336ae55476ecc0065ee5c87f7
Dominik Jain <d.jain@appliedai.de>
2024-06-10 15:50:48 +0200

    Simplify writing of results using sensai ResultWriter

---
+++ src/penai/utils/io.py
@@ -0,0 +1,17 @@
+import sensai.util.io as sensai_io
+
+
+class ResultWriter(sensai_io.ResultWriter):
+    def write_text_file(
+        self,
+        filename_suffix: str,
+        content: str,
+        extension_to_add: str | None = None,
+        content_description: str = "text file",
+    ):
+        p = self.path(filename_suffix, extension_to_add=extension_to_add)
+        if self.enabled:
+            self.log.info(f"Saving {content_description} to {p}")
+            with open(p, "w") as f:
+                f.write(content)
+        return p
