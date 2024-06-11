20562ec1f3739a405afaf41378baa7e6caa58efa
Dominik Jain <d.jain@appliedai.de>
2024-06-11 16:25:57 +0200

    Apply simplified loading of Page SVGs

--- scripts/multimodal_query.py
+++ scripts/multimodal_query.py
@@ -1,20 +1,18 @@
 from typing import cast
 
+from sensai.util import logging
+
 from penai.llm.conversation import Conversation, HumanMessageBuilder
 from penai.llm.llm_model import RegisteredLLM
 from penai.registries.projects import SavedPenpotProject
-from penai.registries.web_drivers import RegisteredWebDriver
-
 from penai.render import WebDriverSVGRenderer
 
 if __name__ == '__main__':
-    saved_penpot_project = SavedPenpotProject.INTERACTIVE_MUSIC_APP
-    penpot_project = saved_penpot_project.load(pull=True)
-    main_file = penpot_project.get_main_file()
-    page = main_file.get_page_by_name("Interactive music app")
-    page.svg.retrieve_and_set_view_boxes_for_shape_elements(RegisteredWebDriver.CHROME)
+    logging.configure()
 
-    shape = page.svg.get_shape_by_name("ic_equalizer_48px-1")
+    page_svg = SavedPenpotProject.INTERACTIVE_MUSIC_APP.load_page_svg_with_viewboxes("Interactive music app")
+
+    shape = page_svg.get_shape_by_name("ic_equalizer_48px-1")
     svg = shape.to_svg()
     svg.strip_penpot_tags()
 
--- scripts/svg_variations_demo.py
+++ scripts/svg_variations_demo.py
@@ -1,14 +1,14 @@
+from sensai.util import logging
+
 from penai.registries.projects import SavedPenpotProject
 from penai.registries.web_drivers import RegisteredWebDriver
 from penai.variations.svg_variations import SVGVariationsGenerator
 
 if __name__ == '__main__':
-    saved_penpot_project = SavedPenpotProject.INTERACTIVE_MUSIC_APP
-    penpot_project = saved_penpot_project.load(pull=True)
-    main_file = penpot_project.get_main_file()
-    page = main_file.get_page_by_name("Interactive music app")
-    page.svg.retrieve_and_set_view_boxes_for_shape_elements(RegisteredWebDriver.CHROME)
-    shape = page.svg.get_shape_by_name("ic_equalizer_48px-1")
+    logging.configure()
+
+    page_svg = SavedPenpotProject.INTERACTIVE_MUSIC_APP.load_page_svg_with_viewboxes("Interactive music app")
+    shape = page_svg.get_shape_by_name("ic_equalizer_48px-1")
 
     var_gen = SVGVariationsGenerator(shape=shape, semantics="equalizer")
     variations = var_gen.create_variations()
