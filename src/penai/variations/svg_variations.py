acf202a2b35bc8bd985c281341952870407f2fac
Dominik Jain <d.jain@appliedai.de>
2024-06-12 17:54:26 +0200

    Refactor variations generation

--- src/penai/variations/svg_variations.py
+++ src/penai/variations/svg_variations.py
@@ -1,9 +1,9 @@
 import logging
 import re
 import uuid
-from collections.abc import Callable
-from enum import Enum, StrEnum
+from enum import StrEnum
 from pathlib import Path
+from typing import Self
 
 from sensai.util.logging import datetime_tag
 
@@ -12,54 +12,59 @@ from penai.llm.conversation import Conversation, Response
 from penai.llm.llm_model import RegisteredLLM
 from penai.svg import SVG, PenpotShapeElement
 from penai.types import PathLike
-from penai.utils.io import ResultWriter
+from penai.utils.io import ResultWriter, fn_compatible
 
 cfg = get_config()
 
 log = logging.getLogger(__name__)
 
 
-class VariationLogicSnippet(StrEnum):
+class VariationInstructionSnippet(StrEnum):
     SHAPES_COLORS_POSITIONS = (
         "Modify shapes, foreground colors and relative positioning, "
         "but stay close to the original design. "
     )
 
 
-def get_default_variation_prefix(num_variations: int) -> str:
-    """Returns the default prefix for prompts that create variations."""
-    return f"Create {num_variations} variations of the SVG."
+PROMPT_FORMAT_DESCRIPTION = (
+    "For each variation, create a level 2 heading (markdown prefix `## `) that names"
+    "the variation followed by the respective code snippet."
+)
 
 
-def get_default_variations_format_description() -> str:
-    """Returns the default format description for prompts that create variations."""
-    return """For each variation, create a level 2 heading (markdown prefix `## `) that names
-the variation followed by the respective code snippet."""
+class VariationsPrompt:
+    def __init__(self, text: str, _private: int):
+        if _private != 42:
+            raise ValueError(
+                "This class should not be instantiated directly. Use VariationsPromptBuilder instead."
+            )
+        self.text = text
 
 
-def get_default_variations_prompt(
-    num_variations: int = 5,
-    variation_logic: str | VariationLogicSnippet = VariationLogicSnippet.SHAPES_COLORS_POSITIONS,
-) -> str:
-    """Returns the default prompt for variations."""
-    prefix = get_default_variation_prefix(num_variations)
-    format_description = get_default_variations_format_description()
-    return f"{prefix}\n{variation_logic}\n{format_description}"
-
-
-class VariationPromptFactory(Enum):
-    DEFAULT = "DEFAULT"
-
-    def get_factory(self) -> Callable[[int, str], str]:
-        """Returns the default prompt factory for the variation prompt.
-
-        The factory will map the number of variations and the variation logic to a prompt string.
+class VariationsPromptBuilder:
+    def __init__(self, num_variations: int | None):
+        """:param num_variations: the number of variations to create; if None, the number is not specified and should be made explicit in
+        customized variation instructions
         """
-        match self:
-            case VariationPromptFactory.DEFAULT:
-                return get_default_variations_prompt
-            case _:
-                raise NotImplementedError(self)
+        num_variations_text = "" if num_variations is None else f"{num_variations} "
+        self._prompt_1_create_variations = f"Create {num_variations_text}variations of the SVG."
+        self._prompt_2_variation_instructions: str | VariationInstructionSnippet = VariationInstructionSnippet.SHAPES_COLORS_POSITIONS
+
+    def with_variation_instructions(self, instructions: str | VariationInstructionSnippet) -> Self:
+        """:param instructions: instructions on how to generate variations.
+            If no number of variations is specified at construction, it should include specific instructions on which variations to generate.
+        :return:
+        """
+        self._prompt_2_variation_instructions = instructions
+        return self
+
+    def build(self) -> VariationsPrompt:
+        return VariationsPrompt(
+            f"{self._prompt_1_create_variations}\n"
+            f"{self._prompt_2_variation_instructions}\n"
+            f"{PROMPT_FORMAT_DESCRIPTION}",
+            42,
+        )
 
 
 def transform_generated_svg_code(svg_code: str) -> str:
@@ -114,6 +119,8 @@ class SVGVariations:
     def to_html(self, width_style: str = "60%") -> str:
         html = "<html><body>"
         html += f'<div style="width:{width_style}">'
+        html += "<h1>Original</h1>"
+        html += self.original_svg.to_string()
         html += "<h1>Variations</h1>"
         for name, svg in self.variations_dict.items():
             html += f"<h2>{name}</h2>"
@@ -156,7 +163,7 @@ class SVGVariationsGenerator:
         self.svg.strip_penpot_tags()
         self.verbose = verbose
         self.model = model
-        responses_dir = Path(persistence_base_dir / shape.name)
+        responses_dir = Path(persistence_base_dir / fn_compatible(shape.name))
         if persistence_add_timestamp:
             responses_dir = responses_dir / datetime_tag()
         self.result_writer = ResultWriter(responses_dir, enabled=persistence_enabled)
@@ -173,54 +180,49 @@ class SVGVariationsGenerator:
             f"```{self.svg.to_string()}```"
         )
 
-    def create_variations(
+    def create_variations_for_prompt(
         self,
-        num_variations: int = 5,
-        variation_logic: str
-        | VariationLogicSnippet = VariationLogicSnippet.SHAPES_COLORS_POSITIONS,
+        variations_prompt: VariationsPrompt,
     ) -> SVGVariations:
-        refactoring_prompt = self.get_svg_refactoring_prompt()
-        logic_prompt = get_default_variations_prompt(num_variations, variation_logic)
-
         conversation = self._create_conversation()
-        refactoring_response = conversation.query_text(refactoring_prompt)
+        refactoring_response = conversation.query_text(self.get_svg_refactoring_prompt())
+
+        variations_response = conversation.query(variations_prompt.text)
+        variations_dict = variations_response.get_variations_dict()
+        variations = SVGVariations(self.svg, variations_dict, conversation)
 
         self.result_writer.write_text_file(
             "response_refactoring.md",
             refactoring_response,
             content_description="SVG refactoring response",
         )
-
-        variations_response = conversation.query(logic_prompt)
-
         self.result_writer.write_text_file(
             "response_variations.md",
             variations_response.text,
             content_description="variations response",
         )
-
         self.result_writer.write_text_file(
             "full_conversation.txt",
             conversation.get_full_conversation_string(),
             content_description="full conversation",
         )
-
-        variations_dict = variations_response.get_code_in_sections(2)
-        variations = SVGVariations(self.svg, variations_dict, conversation)
         self.result_writer.write_text_file(
             "response_variations.html",
             variations.to_html(),
             content_description="variations response as HTML",
         )
+
         return variations
 
-    # TODO: we can save tokens by branching off after the refactoring query instead of resetting
-    def query_free_prompt(
+    def create_variations(
         self,
-        prompt: str,
-        include_svg_refactoring_query: bool = True,
-    ) -> tuple[SVGVariationsResponse, SVGVariationsConversation]:
-        conversation = self._create_conversation()
-        if include_svg_refactoring_query:
-            conversation.query(self.get_svg_refactoring_prompt())
-        return conversation.query(prompt), conversation
+        num_variations: int = 5,
+        variation_logic: str
+        | VariationInstructionSnippet = VariationInstructionSnippet.SHAPES_COLORS_POSITIONS,
+    ) -> SVGVariations:
+        prompt = (
+            VariationsPromptBuilder(num_variations)
+            .with_variation_instructions(variation_logic)
+            .build()
+        )
+        return self.create_variations_for_prompt(prompt)
