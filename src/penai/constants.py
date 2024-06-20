6e9108973c52ffb45095dd8691069a62be6f2671
Kristian Klemon <k.klemon@appliedai.de>
2024-06-20 11:06:02 +0200

    Add doc string to constants module

--- src/penai/constants.py
+++ src/penai/constants.py
@@ -1,3 +1,5 @@
+"""Constants used throughout the project."""
+
 # Penpot generally uses Google Fonts fonts but some of the older "default" fonts apparently
 # have been hardcoded under a different name. We therefore need to recognize these cases
 # and map them to the correct Google Fonts font family name.
