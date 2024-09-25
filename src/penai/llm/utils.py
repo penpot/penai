6d5358c64ccc10bc97fc4b46cfbb56b710b2f7b1
Kristian Klemon <k.klemon@appliedai.de>
2024-09-25 13:17:03 +0200

    Implement prompt visualizer

---
+++ src/penai/llm/utils.py
@@ -0,0 +1,72 @@
+from collections import defaultdict
+from html import escape
+from pathlib import Path
+from textwrap import dedent
+
+from langchain_core.messages import AIMessage, BaseMessage, HumanMessage, SystemMessage
+
+label_by_message_type: dict[type[BaseMessage], str] = defaultdict(lambda: "📝 Message")
+label_by_message_type.update(
+    {
+        AIMessage: "🤖 AI Message",
+        HumanMessage: "👤 Human Message",
+        SystemMessage: "🖥️ System Message",
+    }
+)
+
+default_stylesheet_path = (
+    (Path(__file__) / "../../../../data/style.css").resolve().absolute()
+)
+
+
+class PromptVisualizer:
+    def __init__(self, stylesheet_path: str | Path | None = default_stylesheet_path):
+        if stylesheet_path is not None:
+            self.stylesheet = (
+                "<style>\n" + Path(stylesheet_path).read_text() + "\n</style>"
+            )
+        else:
+            self.stylesheet = ""
+
+    def _visualize_message(self, message: BaseMessage) -> str:
+        label = label_by_message_type[type(message)]
+
+        sections = []
+
+        for section in message.content:
+            assert isinstance(section, dict) and "type" in section
+
+            match section["type"]:
+                case "text":
+                    sections.append(escape(section["text"]).replace("\n", "<br>"))
+                case "image_url":
+                    sections.append(f"<img src='{section['image_url']['url']}' />")
+                case _:
+                    raise ValueError(
+                        f"Unsupported message section type: {section['type']}"
+                    )
+
+        paragraphs = (f"<p>{section}</p>" for section in sections)
+
+        return f"<h2>{label}</h2>\n\n" + "\n".join(paragraphs)
+
+    def _build_html(self, content: str) -> str:
+        return dedent(f"""
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    {self.stylesheet}
+                </head>
+                <body>
+                    {content}
+                </body>
+            </html>
+        """)
+
+    def visualize_message(self, message: BaseMessage) -> str:
+        return self._build_html(self._visualize_message(message))
+
+    def visualize_messages(self, messages: list[BaseMessage]) -> str:
+        return self._build_html(
+            "</br>".join(self._visualize_message(message) for message in messages)
+        )
