0f02017327f5626d1982a9a6cbce095dd18b1a5b
Kristian Klemon <k.klemon@appliedai.de>
2024-10-24 19:51:05 +0200

    Skip test_can_be_loaded test

--- test/penai/test_registries.py
+++ test/penai/test_registries.py
@@ -1,8 +1,11 @@
+import pytest
+
 from penai.registries.projects import SavedPenpotProject
 
 
 class TestPenpotProjectRegistry:
     @staticmethod
+    @pytest.mark.skip(reason="may fail due to a upstream bug in accrs")
     def test_can_be_loaded(example_project: SavedPenpotProject) -> None:
         loaded_project = example_project.load(pull=True)
         assert len(loaded_project.files) > 0
